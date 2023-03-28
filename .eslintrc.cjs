3558fe283b50002a3144c88128031cbcdb76753b
peacepiece <peacepiece7@naver.com>
2023-03-28 17:36:08 +0900

    chore : add svg icons & imgs
    블루 보틀 메인 페이지 에서  쓰이는 svg icons, images 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,16 +10,20 @@ module.exports = {
       files: ['*.html'],
       parser: '@html-eslint/parser',
       extends: ['plugin:@html-eslint/recommended'],
+      rules: {
+        '@html-eslint/indent': ['error', 2], // html indent는 default : 4라서 따로 적어줘야합니다.
+      },
     },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: 'latest', // ecma 최신 버전을 사용합니다.
+    sourceType: 'module', // module (import, export) 방식을 사용합니다.
   },
   rules: {
     'no-console': 'warn', // console 사용시 warning이 나타납니다.
     'linebreak-style': 'off', // crlf, lf 상관없도록 변경합니다.
     semi: ['error', 'never'], // semicolon 제거합니다.
     'no-underscore-dangle': ['warn', { allowAfterThis: true }], // this._bar()는 가능하고 그 외는 warning이 나타납니다.
+    'max-len': ['warn', { code: 120, tabWidth: 2 }],
   },
 }
