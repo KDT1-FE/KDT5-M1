c7af5693508749ddc1f698157847003072d1dd57
seodonguk <tjehddnr997@gmail.com>
2023-04-02 23:29:34 +0900

    add lit links

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -244,7 +244,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49945" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64594" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
