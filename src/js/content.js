a5b6d9decb2159d35236670104a4be48353e5bc7
pildrums <there7788@gmail.com>
2023-04-06 22:03:55 +0900

    Fix leftContent.style.top in line 15 > onScrollFixedCard 함수 안에 leftContent.style.top의 값을 635px로 수정 > 각 로직에 대한 주석 작성

--- src/js/content.js
+++ src/js/content.js
@@ -2,14 +2,17 @@ const leftContent = document.getElementById("left-box");
 const topBtn = document.querySelector(".top-button");
 
 function onScrollFixedCard() {
+  // 스크롤해서 350px 지점부터 좌측 카드 스크롤해서 따라옴
+  // TODO: classlist.add 사용 (현재 작동 X)
   if (window.scrollY >= 350) {
     leftContent.style.position = "fixed";
     leftContent.style.top = "72px";
     leftContent.style.width = "630px";
     leftContent.style.height = "700px";
+    // 스크롤 1000px 지점에서 하단 고정
     if (window.scrollY >= 1000) {
       leftContent.style.position = "absolute";
-      leftContent.style.top = "640px";
+      leftContent.style.top = "635px";
     }
     console.dir(window.scrollY);
   } else {
@@ -18,6 +21,7 @@ function onScrollFixedCard() {
   }
 }
 
+// Top 버튼 이벤트 함수
 function onScrollTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
