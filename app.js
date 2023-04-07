e0fafe7b6e62caad39caf4a99b415c359a3dfdfb
BearHumanS <bye0651@naver.com>
2023-04-08 02:20:42 +0900

    Code Refactoring

--- app.js
+++ app.js
@@ -1,56 +1,83 @@
+/* appsDropdown */
 const appsButton = document.querySelector('.apps-button');
 const appsDropdown = document.querySelector('.apps-dropdown');
-const drop = document.querySelector(".option-item");
-const dropdownContent = document.querySelector(".dropdown-content");
 
-appsButton.addEventListener('click', () => {
-  if (appsDropdown.style.display === 'block') {
-    appsDropdown.style.display = 'none';
+appsButton.addEventListener('click', e => {
+  e.stopPropagation();
+  if(appsDropdown.classList.contains('show')) {
+    hide();
   } else {
-    appsDropdown.style.display = 'block';
+    show();
   }
 });
 
-document.addEventListener("click", function (e) {
-  if (!appsButton.contains(e.target)) {
-    appsDropdown.style.display = 'none';
-  }
+appsDropdown.addEventListener('click', e => {
+  e.stopPropagation();
 })
 
-drop.addEventListener("click", function() {
-  if (dropdownContent.style.display === 'block') {
-    dropdownContent.style.display = 'none';
+window.addEventListener('click', hide);
+
+function show () {
+  appsDropdown.classList.add('show');
+};
+function hide () {
+  appsDropdown.classList.remove('show');
+}
+
+/* footer_dropDown */
+const drop = document.querySelector('.option-item');
+const dropdownContent = document.querySelector('.dropdown-content');
+
+drop.addEventListener('click', e => {
+  e.stopPropagation();
+  if(dropdownContent.classList.contains('show-item')) {
+    footerHide();
   } else {
-    dropdownContent.style.display = 'block';
+    footerShow();
   }
 });
 
-document.addEventListener("click", function (e) {
-  if (!drop.contains(e.target)) {
-    dropdownContent.style.display = 'none';
-  }
+dropdownContent.addEventListener('click', e => {
+  e.stopPropagation();
 });
 
-const dark= document.querySelector(".dark-mode");
+window.addEventListener('click', footerHide);
 
-const body = document.querySelector("body");
-const footer = document.querySelector("footer");
-const header = document.querySelector("header");
-const sun = document.querySelector(".sun");
+function footerShow () {
+  dropdownContent.classList.add('show-item');
+};
+function footerHide () {
+  dropdownContent.classList.remove('show-item');
+};
 
-dark.addEventListener("click", function () {
-  const change = document.querySelector("#change");
-    if(change.value === "어두운 테마: 사용 안함") {
-        body.classList.add("body-night");
-        footer.classList.add("footer-night")
-        header.classList.add("header-night");
-        change.value = "어두운 테마: 사용";
+/* Dark Mode */
+const dark= document.querySelector('.dark-mode');
+const body = document.querySelector('body');
+const footer = document.querySelector('footer');
+const header = document.querySelector('header');
+const sun = document.querySelector('.sun');
+
+dark.addEventListener('click', function () {
+  const change = document.querySelector('#change');
+    if(change.value === '어두운 테마: 사용 안함') {
+        add();
+        change.value = '어두운 테마: 사용';
         document.querySelector('.material-icons.sun').textContent = 'dark_mode';
     } else {
-        body.classList.remove("body-night");
-        footer.classList.remove("footer-night");
-        header.classList.remove("header-night");
-        change.value = "어두운 테마: 사용 안함";
+        remove();
+        change.value = '어두운 테마: 사용 안함';
         document.querySelector('.material-icons.sun').textContent = 'light_mode';
     }
 });
+
+
+function add () {
+  body.classList.add('body-night');
+  footer.classList.add('footer-night')
+  header.classList.add('header-night');
+};
+function remove () {
+  body.classList.remove('body-night');
+        footer.classList.remove('footer-night');
+        header.classList.remove('header-night');
+};
